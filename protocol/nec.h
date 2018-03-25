9876fcb1e9ed1556c38681e2847a1d01bc6ab1e7
Tuomo Jauhiainen <juppi@project-apollo.co.uk>
2018-03-25 13:51:39 +0300

    Fix incorrect header path.

--- protocol/nec.h
+++ protocol/nec.h
@@ -1,7 +1,7 @@
 #ifndef __PROTOCOL_NEC_H
 #define __PROTOCOL_NEC_H
 
-#include "ir.h"
+#include "ir/ir.h"
 
 bool nec_decode(message_t *result, volatile uint32_t *buffer, volatile uint8_t buffer_length);
 
